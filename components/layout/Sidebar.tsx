9de9f45983ed9647f50ba8b455f9cbba0f5e7a38
Vaibhav Jaiswal <vaibhav.jaiswal.vj91@gmail.com>
2024-07-20 17:14:48 +0530

    database and fetching user is done

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
+import { BiLogOut } from "react-icons/bi";
+import SidebarTweetButton from "./SidebarTweetButton";
+const Sidebar = () => {
+  const items = [
+    {
+      label: "Home",
+      href: "/",
+      icon: BsHouseFill,
+    },
+    {
+      label: "Notifications",
+      href: "/notification",
+      icon: BsBellFill,
+    },
+    {
+      label: "Profile",
+      href: "/users/123",
+      icon: FaUser,
+    },
+  ];
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-row items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
+          <SidebarItem
+            onClick={() => {}}
+            icon={BiLogOut}
+            label="logout"
+            href={""}
+          />
+          <SidebarTweetButton />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Layout from "../../components/Layout";
+import Modal from "../../components/Modal";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Modal
+        onClose={() => {}}
+        onSubmit={() => {}}
+        title="Test Modal"
+        actionLabel="Submit"
+      />
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
